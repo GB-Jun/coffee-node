bf8d1cc24dd689436a747ebe1e27d5ef392e4bc5
chia <chia87616@gmail.com>
2022-07-26 01:26:14 +0800

    member routes

--- routes/member/member.js
+++ routes/member/member.js
@@ -1,29 +1,32 @@
 const express = require("express");
 const router = express.Router(); // 建立route物件
 const db = require(__dirname + "/../../modules/mysql-connect");
+const upload = require(__dirname + "/../../modules/upload-images");
 
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 
-router.post('/login', (req, res) => {
+
+
+router.post('/login', upload.none(), async(req, res) => {
 
     const output = {
         success: false,
         error: '',
         code: 0,
     };
-    const sql = "SELECT * FROM member WHERE member_account=?";
-    const [result] = db.query(sql, [req.body.member_account]);
+    const sql = "SELECT * FROM `member` WHERE `member_account`=?";
+    const [result] = await db.query(sql, [req.body.member_account]);
+
 
     if (!result.length) {
-        // 沒有陣列長度代表比對不到資料庫裡的帳號，帳號錯誤
         output.code = 401;
         output.error = '帳密錯誤';
         return res.json(output);
     }
 
-    output.success = bcrypt.compare(req.body.member_password, result[0].pass_hash);
-    console.log(bcrypt.compare(req.body.member_password, result[0].pass_hash));
+    output.success = bcrypt.compare(req.body.member_password, result[0].member_password);
+
     if (!output.success) {
         // 密碼錯誤
         output.code = 402;
@@ -40,15 +43,45 @@ router.post('/login', (req, res) => {
             token,
             account: result[0].member_account,
         };
-
+        output.success = true;
     }
 
     res.json(output);
 });
 
-// ------------------ 頭貼上傳 ------------------ 
-// router.post('/upload', upload.single('avatar'),(req,res)=>{
-//     res.json(req.file);
-// })
+
+
+router.post('/sign-up', async (req, res) => {
+
+    const output = {
+        success: false,
+        error: '',
+    };
+
+    const sql = "INSERT INTO `member`(`member_name`, `member_account`, `member_password`) VALUES (?, ?, ?)";
+    const {member_name, member_account, member_password} = req.body;
+    const [result] = await db.query(sql, [member_name, member_account, member_password]);
+
+    req.body.member_password = bcrypt.hashSync(req.body.member_password, 10);
+
+    if (!result.length) {
+        output.error = '註冊失敗';
+        return res.json(output);
+    }else{
+        output.success = true;
+    }
+
+
+});
+
+
+
+router.get('/order-history', async (req, res) => {
+    const sql = "SELECT `order_sid`, `order_time`, `order_member_id`, `order_price`, `order_id` FROM `order` WHERE 1";
+    const [results] = await db.query(sql);
+
+    res.json(results);
+});
+
 
 module.exports = router;
