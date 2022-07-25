3835c3b502d6b33461443f26c31ed092084d5aee
minchia0616 <chia87616@gmail.com>
2022-07-25 16:41:06 +0800

    member

--- routes/member/member.js
+++ routes/member/member.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router(); // 建立route物件
 const db = require(__dirname + "/../../modules/mysql-connect");
 
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+
 router.post('/login',(req, res)=>{
 
     const output = {
@@ -10,17 +13,17 @@ router.post('/login',(req, res)=>{
         code: 0,
     };
     const sql = "SELECT * FROM member WHERE member_account=?";
-    const [result1] = db.query(sql, [req.body.member_account]);
+    const [result] = db.query(sql, [req.body.member_account]);
 
-    if(! result1.length){
+    if(! result.length){
         // 沒有陣列長度代表比對不到資料庫裡的帳號，帳號錯誤
         output.code = 401;
         output.error = '帳密錯誤'
-        return res.json(output)
+        return res.json(output);
     }
 
-    output.success = bcrypt.compare(req.body.member_password, result1[0].pass_hash);
-    console.log( bcrypt.compare(req.body.member_password, result1[0].pass_hash));
+    output.success = bcrypt.compare(req.body.member_password, result[0].pass_hash);
+    console.log( bcrypt.compare(req.body.member_password, result[0].pass_hash));
     if(! output.success){
         // 密碼錯誤
         output.code = 402;
@@ -28,20 +31,24 @@ router.post('/login',(req, res)=>{
     }else {
         // 成功登入
         const token = jwt.sign({
-            sid: result1[0].member_sid,
-            account: result1[0].member_account,
+            sid: result[0].member_sid,
+            account: result[0].member_account,
         }, process.env.JWT_SECRET);
 
-        // 前端無法解密token，所以要把需要呈現到頁面上的東西(帳號之類的)一起傳給前端
         output.data = {
-            sid: result1[0].member_sid,
+            sid: result[0].member_sid,
             token,
-            account: result1[0].member_account,
+            account: result[0].member_account,
         };
 
     }
 
     res.json(output);
 });
+
+// ------------------ 頭貼上傳 ------------------ 
+router.post('/upload', upload.single('avatar'),(req,res)=>{
+    res.json(req.file);
+})
 
 module.exports = router;
