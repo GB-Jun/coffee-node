870a80817cfab91b42630e37218ea3b138940461
chia <chia87616@gmail.com>
2022-07-29 01:24:18 +0800

    jwt token

--- routes/member/member.js
+++ routes/member/member.js
@@ -42,12 +42,26 @@ router.post('/login', upload.none(), async(req, res) => {
         const token = jwt.sign({
             sid: result[0].member_sid,
             account: result[0].member_account,
+            name: result[0].member_name,
+            birthday: result[0].member_birthday,
+            mobile: result[0].member_mobile,
+            address: result[0].member_address,
+            mail: result[0].member_mail,
+            level: result[0].member_level,
+            avatar: result[0].avatar,
         }, process.env.JWT_SECRET);
 
         output.data = {
             sid: result[0].member_sid,
             token,
             account: result[0].member_account,
+            name: result[0].member_name,
+            birthday: result[0].member_birthday,
+            mobile: result[0].member_mobile,
+            address: result[0].member_address,
+            mail: result[0].member_mail,
+            level: result[0].member_level,
+            avatar: result[0].avatar,
         };
         output.success = true;
     }
@@ -95,7 +109,7 @@ router.post('/sign-up', async (req, res) => {
 
 // --------------------- 讀取會員資料 ---------------------
 router.get('/api/user-list', async (req, res) => {
-    const sql = "SELECT `member_sid`,`member_name`, `member_nickname`, `member_account`, `member_password`, `member_birthday`, `member_mobile`, `member_address`, `member_mail`, `avatar` FROM `member` WHERE `member_sid`=1";
+    const sql = "SELECT `member_sid`,`member_name`, `member_nickname`, `member_account`, `member_password`, `member_birthday`, `member_mobile`, `member_address`, `member_mail`, `avatar` FROM `member` WHERE `member_sid`= 1";
     const [results] = await db.query(sql);
 
     res.json(results);
@@ -104,16 +118,18 @@ router.get('/api/user-list', async (req, res) => {
 // --------------------- 修改密碼 ---------------------
 router.post('/api/edit-password', async (req, res) => {
 
+    console.log(res.locals.loginUser.sid);
+
     const output = {
         success: false,
         error: '',
     };
 
-    const sql = " SELECT `member_password` FROM `member` WHERE `member_sid`= 105";
+    const sql = " SELECT `member_password` FROM `member` WHERE `member_sid`= 1463";
     const [result] = await db.query(sql);
-    const newSql = " UPDATE `member` SET `member_password`= ? WHERE `member_sid`= 105";
+    const newSql = " UPDATE `member` SET `member_password`= ? WHERE `member_sid`= 1463";
 
-    const password = await bcrypt.compareSync(req.body.member_password,result[0].member_password);
+    const password = bcrypt.compareSync(req.body.member_password,result[0].member_password);
     output.success = password;
     console.log(password);
 
