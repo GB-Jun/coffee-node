c70c742bcbf8bc70e038e94a12d6a05216f0e712
GB-Jun <windgb369@hotmail.com>
2022-07-28 16:41:34 +0800

    add output

--- routes/products/products.js
+++ routes/products/products.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const router = express.Router(); // 建立route物件
 const db = require(__dirname + "/../../modules/mysql-connect");
-const { toDateString, toDateTimeString } = require(__dirname +
-    "/../../modules/date-tools");
-const moment = require("moment-timezone");
 const Joi = require("joi");
 const uploads = require(__dirname + "/../../modules/upload-images");
 
 const getListHandler = async (req, res) => {
     let output = {
-        perPage: 5,
+        perPage: 8,
         page: 1,
         totalRows: 0,
         totalPages: 0,
@@ -17,34 +14,21 @@ const getListHandler = async (req, res) => {
         error: "",
         query: {},
         rows: [],
+        totalData:[],
         search: "",
         showTest: "",
+        sid: 0
     };
-    let page = +req.query.page || 1;
 
-    // let beginDate = req.query.beginDate || "";
-    // let endDate = req.query.endDate || "";
+    let page = +req.query.page || 1;
     let search = req.query.search || "";
     let where = " WHERE 1 ";
+    
     if (search) {
         where += ` AND name LIKE ${db.escape("%" + search + "%")} `; // bug
         output.query.search = search;
         output.showTest = db.escape("%" + search + "%");
     }
-    // if (beginDate) {
-    //     const mo = moment(beginDate);
-    //     if (mo.isValid()) {
-    //         where += ` AND birthday >= '${mo.format("YYYY-MM-DD")}'  `;
-    //         output.query.beginDate = mo.format("YYYY-MM-DD");
-    //     }
-    // }
-    // if (endDate) {
-    //     const mo = moment(endDate);
-    //     if (mo.isValid()) {
-    //         where += ` AND birthday <= '${mo.format("YYYY-MM-DD")}'  `;
-    //         output.query.endDate = mo.format("YYYY-MM-DD");
-    //     }
-    // }
     output.showTest = where; // 用於檢查篩選的結果, 可以在api查看資訊來除錯
 
     if (page < 1) {
@@ -55,7 +39,7 @@ const getListHandler = async (req, res) => {
         // 如果用?來寫, 代表前面的路徑都一樣
     }
 
-    const sqlnum = `SELECT COUNT(1) totalRows FROM products ${where} ORDER BY products_sid DESC `;
+    const sqlnum = `SELECT COUNT(1) totalRows FROM products ${where} ORDER BY products_sid ASC `;
     // const [result01] = await db.query(sqlnum);
     // const [[result01]] = await db.query(sqlnum);
     const [[{ totalRows }]] = await db.query(sqlnum);
@@ -70,7 +54,7 @@ const getListHandler = async (req, res) => {
             return output;
         }
 
-        const sqlData = `SELECT * FROM products ${where} ORDER BY products_sid DESC LIMIT ${
+        const sqlData = `SELECT * FROM products ${where} ORDER BY products_sid ASC LIMIT ${
             (page - 1) * output.perPage
         }, ${output.perPage}`;
         const [result02] = await db.query(sqlData);
@@ -78,6 +62,9 @@ const getListHandler = async (req, res) => {
         // 也能在主層index.js那邊寫template helper function, 讓function大家都能用
         // result02.forEach((el) => (el.birthday2 = toDateString(el.birthday)));
         output.rows = result02;
+        const totoalDataSql = `SELECT * FROM products ORDER BY products_sid ASC`
+        const [resultTotal] = await db.query(totoalDataSql)
+        output.totalData = resultTotal
     }
 
     output.code = 200;
@@ -87,72 +74,14 @@ const getListHandler = async (req, res) => {
     return output;
 };
 
-router.use((req, res, next) => {
-    // 直接用top-level來擋沒有登入管理者
-    /*
-    if(! req.session.admin){
-        return res.redirect('/');
-    }
-    */
 
+//----------------------------------------------------------------------------------
+
+router.use((req, res, next) => {
+    // top-level middleware
     next();
 });
 
-// router.get("/add", async (req, res) => {
-//     if (!req.session.admin) {
-//         return res.redirect("/");
-//     }
-//     res.render("address-book/add");
-// });
-// router.post("/add", uploads.none(), async (req, res) => {
-//     if (!req.session.admin) {
-//         return res.json({ success: false, error: "請先登入" });
-//         // 要記得post也要擋
-//     }
-    // const schema = Joi.object({
-    //     name: Joi.string().min(3).max(30).required().label("姓名必填"),
-    //     email: Joi.string().email().required(),
-    //     mobile: Joi.string(),
-    //     birthday: Joi.any(),
-    //     // any 後面還可以設定optional()
-    //     address: Joi.string(),
-    // });
-
-    // console.log(schema.validate(req.body, { abortEarly: false }));
-
-    // const sql =
-    //     "INSERT INTO `address_book`(`name`, `email`, `mobile`, `birthday`, `address`, `created_at`) VALUES (?,?,?,?,?, NOW())";
-    // const { name, email, mobile, birthday, address } = req.body;
-    // const [result] = await db.query(sql, [
-    //     name,
-    //     email,
-    //     mobile,
-    //     birthday,
-    //     address,
-    // ]);
-
-//     const sql = "INSERT INTO `address_book` SET ?";
-//     const inserData = { ...req.body, created_at: new Date() };
-//     const [result] = await db.query(sql, [inserData]);
-// });
-
-// router.get("/", async (req, res) => {
-//     const output = await getListHandler(req, res);
-//     switch (output.code) {
-//         case 410:
-//             return res.redirect(`?page=1`);
-//             break;
-//         case 420:
-//             return res.redirect(`?page=${output.totalPages}`);
-//             break;
-//     }
-//     // 在不同的權限(有沒有登入) 能看到的東西不一樣
-//     if (!req.session.admin) {
-//         res.render("address-book/main-noadmin", output);
-//     } else {
-//         res.render("address-book/main", output);
-//     }
-// });
 
 router.get("/api", async (req, res) => {
     const output = await getListHandler(req, res);
@@ -160,21 +89,18 @@ router.get("/api", async (req, res) => {
     res.json(output);
 });
 
+router.get("/api/detail/:sid", async (req, res) => {
+    const output = await getListHandler(req, res);
+    output.payload = res.locals.payload
+    res.json(output);
+});
 
-// 有 token 才給過
-// router.get('/api-auth', async (req, res)=>{
-//     let output = {
-//         success: false,
-//         error: ''
-//     };
+router.post("/api", async (req, res) => {
+    const output = await getListHandler(req, res);
+    output.payload = res.locals.payload
+    res.json(output);
+});
 
-//     if(res.locals.loginUser && res.locals.loginUser.account){
-//         output = {...(await getListHandler(req, res)), success: true};
+//------------------------------------------------------------------------
 
-//     } else {
-//         output.error = '沒有授權';
-//     }
-//     output.loginUser = res.locals.loginUser;
-//     res.json(output);
-// });
 module.exports = router;
