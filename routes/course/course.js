99332b393fbd22f6f0523b5f5dc8737f09284f24
min <a0955361101@gmail.com>
2022-07-31 22:48:44 +0800

    edit api

--- routes/course/course.js
+++ routes/course/course.js
@@ -48,10 +48,10 @@ router.get('/FK-get', async (req, res) => {
 // INSERT INTO `course`(`course_name`, `course_price`, `course_level`, `course_img_s`, `course_content`, `course_people`, `course_material`) VALUES (?,?,?,?,?,?,?)
 // 新增
 router.post('/add', async (req, res) => {
-    console.log(req.body);
+    console.log(req.body.course_name);
     const { course_name, course_price, course_level, course_img_s, course_content, course_people, course_material } = req.body;
     const sql = "INSERT INTO `course`(`course_name`, `course_price`, `course_level`, `course_img_s`, `course_content`, `course_people`, `course_material`) ";
-    const setSql = `VALUES (${course_name},${course_price},${course_level},\"${course_img_s}\",${course_content},${course_people},${course_material})`;
+    const setSql = `VALUES (\"${course_name}\",${course_price},${course_level},\"${course_img_s}\",\"${course_content}\",\"${course_people}\",\"${course_material}\")`;
     const insertSql = `${sql}${setSql}`;
     const result = await db.query(insertSql);
     console.log(result);
@@ -62,10 +62,8 @@ router.post('/add', async (req, res) => {
 router.post('/addfk', async (req, res) => {
     console.log(req.body);
     const { course_sid, course_date, course_time, course_img_l } = req.body;
-    const { date1, date2 } = course_date;
-    const { time1, time2 } = course_time;
     const sql = "INSERT INTO `course_related`(`course_sid`, `course_date`, `course_time`, `course_img_l`)";
-    const setSql = `VALUES (${course_sid},${date1},${time1},\"${course_img_l}\"),(${course_sid},${date2},${time2},\"${course_img_l}\")`;
+    const setSql = `VALUES (${course_sid},\"${course_date}\",\"${course_time}\",\"${course_img_l}\")`;
     const insertSql = `${sql}${setSql}`;
     const result = await db.query(insertSql);
     console.log(result);
@@ -89,10 +87,22 @@ router.delete('/delete/:sid', async (req, res) => {
 router.put('/edit', async (req, res) => {
     console.log(req.body);
     const { course_name, course_price, course_level, course_img_s, course_content, course_people, course_material, course_sid } = req.body;
-    const sql = `UPDATE course SET course_name = ${course_name}, course_price = ${course_price}, course_level = ${course_level}, course_img_s = ${course_img_s}, course_content = ${course_content}, course_people = ${course_people}, course_material = ${course_material} WHERE course.course_sid = ${course_sid}`;
+    const sql = `UPDATE course SET course_name = \"${course_name}\", course_price = ${course_price}, course_level = ${course_level}, course_img_s = \"${course_img_s}\", course_content = \"${course_content}\", course_people = \"${course_people}\", course_material = \"${course_material}\" WHERE course.course_sid = ${course_sid}`;
     const result = await db.query(sql);
     console.log(result);
     return res.json(result[0].insertId);
+});
+
+// 修改外鍵
+router.put('/editFk', async (req, res) => {
+    console.log(req.body);
+    const { course_sid, course_date, course_time, course_img_l } = req.body;
+    // const { date1, date2 } = course_date;
+    // const { time1, time2 } = course_time;
+    const sql = `UPDATE course_related SET course_date = \"${course_date}\", course_time = \"${course_time}\", course_img_l = \"${course_img_l}\" WHERE course_related.course_sid = ${course_sid};`;
+    const result = await db.query(sql);
+    console.log(result);
+    return res.json(result);
 });
 
 // ------------ 跟LINE PAY 串接的 API -----------
