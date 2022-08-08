55dd90bd933cbf27121999d17a1f00b643efa4ef
Tommy <sky003428@gmail.com>
2022-08-08 17:39:44 +0800

    previewAPI

--- routes/sharing/previewAPI.js
+++ routes/sharing/previewAPI.js
@@ -1,33 +1,90 @@
+const { query } = require("express");
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const db = require(__dirname + "/../../modules/mysql-connect");
 
+const getTitleData = async (q) => {
+    const WHERE = q ? `p.title LIKE '%${q}%' AND` : "";
+    const ORDER = " ORDER BY p.likes DESC"
+    const LIMIT = " LIMIT 8";
+
+
+    const sql = `
+    SELECT 'title' AS type ,p.title AS name ,p.sid AS post_sid ,pi.img_name AS src FROM post p 
+    LEFT JOIN post_img pi ON p.sid = pi.post_sid 
+    WHERE ${WHERE} pi.sort = 1 AND p.delete_state = 0
+    ${ORDER} ${LIMIT};
+    `;
+
+    const [r] = await db.query(sql);
+    return r;
+}
+
+const getNicknameData = async (q) => {
+    const WHERE = q ? `m.member_nickname LIKE '%${q}%'` : "1";
+    const LIMIT = " LIMIT 8";
+
+    // 輸出{type = "nickname",name:nickname,src:avatar,member_sid,post_quantity}
+    // 以關聯子查詢post_quantity做降冪排序,發文數多的會員會排在前面
+    const sql = `
+    SELECT 'nickname' AS type,
+    m.member_nickname AS name,
+    m.avatar AS src,
+    m.member_sid, 
+    (SELECT COUNT(*) posts FROM post WHERE member_sid = p.member_sid) AS post_quantity
+    FROM member m
+    LEFT JOIN post p 
+    ON m.member_sid = p.member_sid 
+    WHERE ${WHERE}
+    GROUP BY member_sid
+    ORDER BY post_quantity DESC ${LIMIT};
+    `;
+
+
+    const [r] = await db.query(sql);
+    console.log(r)
+    return r;
+};
+
+
+const getTagData = async (q) => {
+    const WHERE = q ? `t.name LIKE '%${q}%'` : "1";
+    const LIMIT = " LIMIT 8";
+
+    const sql = `
+    SELECT 'tag' AS type, t.* FROM tag t WHERE ${WHERE} ORDER BY times DESC ${LIMIT};`;
+
+
+    const [r] = await db.query(sql);
+    return r;
+};
+
+
+
 router.get("", async (req, res) => {
-    const { title, tag, member_sid } = req.query;
+    const { queryString, title, tag, member_sid } = req.query;
 
     const output = {
         totalRows: 0,
         rows: [],
-        query: { title, tag, member_sid },
+        query: { queryString },
     }
 
-    let WHERE = "";
-    title ? WHERE += `p.title LIKE '%${title}%' AND` : WHERE += "";
 
-    const sql = `
-    SELECT p.* ,pi.img_name ,pi.sort,m.avatar FROM \`post\` p 
-    LEFT JOIN \`post_img\` pi ON p.sid = pi.post_sid 
-    LEFT JOIN \`member\` m ON p.member_sid = m.member_sid
-    WHERE ${WHERE} pi.sort = 1 AND p.delete_state = 0;
-    `;
+    // // let WHERE = "";
+    // // title ? WHERE += `p.title LIKE '%${title}%' AND` : WHERE += "";
+    // const nicknameWHERE = queryString ? `member_nickname LIKE '%${queryString}%'` : "";
 
-    const [r] = await db.query(sql);
 
-    output.rows = r;
-    output.totalRows = r.length || 0;
-    // console.log(r)
 
-    res.json(output)
+    output.rows = [...output.rows, ...await getTitleData(queryString)];
+    output.rows = [...output.rows, ...await getNicknameData(queryString)];
+    output.rows = [...output.rows, ...await getTagData(queryString)];
+
+
+    output.totalRows = output.rows.length;
+
+    res.json(output);
 });
 
 
