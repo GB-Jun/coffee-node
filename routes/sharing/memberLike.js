2b579193dacad36551bdcf0e504b8c0792912b1b
Tommy <sky003428@gmail.com>
2022-08-11 18:42:52 +0800

    bug fix

--- routes/sharing/memberLike.js
+++ routes/sharing/memberLike.js
@@ -21,14 +21,19 @@ router.get("/:post_sid", async (req, res) => {
     SELECT COUNT(*) AS liked FROM member_likes
     WHERE post_sid = ? AND member_sid = ?;
     `
-    const [[r]] = await db.query(sql, [post_sid, member_sid]);
-    const totalSql = `SELECT likes AS total FROM post WHERE sid = ?`
-    const [[r2]] = await db.query(totalSql, [post_sid]);
-    op.liked = r.liked;
-    op.total = r2.total;
-    op.success = true;
+    try {
+        const [[r]] = await db.query(sql, [post_sid, member_sid]);
+        const totalSql = `SELECT likes AS total FROM post WHERE sid = ?`
+        const [[r2]] = await db.query(totalSql, [post_sid]);
+        op.liked = r.liked;
+        op.success = true;
+        res.json(op);
+    } catch (err) {
+        res.json(err);
+        console.log(err);
+    }
 
-    res.json(op);
+
 });
 
 router.post("/:post_sid", async (req, res) => {
