c018f520b10caa795766cf4dae10926ea5ae9b37
Trista <hua2367@gmail.com>
2022-08-14 14:00:34 +0800

    Coffee.sql 惠優惠券

--- routes/game/Coupon_foruser.js
+++ routes/game/Coupon_foruser.js
@@ -30,16 +30,16 @@ const getListHandler = async (req, res)=>{
     output.member_sid=sid;
 
 
-        const sql=`SELECT coupon.coupon_name,coupon.coupon_money,coupon_receive.end_time,coupon_receive.member_sid FROM coupon_receive JOIN coupon ON coupon_receive.coupon_sid=coupon.sid LEFT JOIN coupon_logs ON coupon_receive.sid=coupon_logs.coupon_receive_sid JOIN member ON coupon_receive.member_sid=member.member_sid WHERE coupon_receive.member_sid=? AND coupon_receive.status=0 AND coupon_receive.end_time> NOW();`;
+        const sql=`SELECT coupon.coupon_name,coupon.coupon_money,coupon_receive.end_time,coupon_receive.member_sid FROM coupon_receive JOIN coupon ON coupon_receive.coupon_sid=coupon.sid LEFT JOIN coupon_logs ON coupon_receive.sid=coupon_logs.coupon_receive_sid JOIN member ON coupon_receive.member_sid=member.member_sid WHERE coupon_receive.member_sid=? AND coupon_receive.status=0 AND coupon_receive.end_time> NOW() ORDER BY end_time DESC;`;
         const [rows_type1]=await db.query(sql,[output.member_sid]);
         output.rows_type1=rows_type1;
         
-        const sql1=`SELECT coupon.coupon_name,coupon.coupon_money,coupon_receive.end_time,coupon_receive.status,coupon_receive.member_sid FROM coupon_receive JOIN coupon ON coupon_receive.coupon_sid=coupon.sid LEFT JOIN coupon_logs ON coupon_receive.sid=coupon_logs.coupon_receive_sid JOIN member ON coupon_receive.member_sid=member.member_sid WHERE coupon_receive.member_sid=1 AND coupon_receive.end_time <NOW() AND coupon_receive.status=0 AND coupon_receive.end_time>0`;
+        const sql1=`SELECT coupon.coupon_name,coupon.coupon_money,coupon_receive.end_time,coupon_receive.status,coupon_receive.member_sid FROM coupon_receive JOIN coupon ON coupon_receive.coupon_sid=coupon.sid LEFT JOIN coupon_logs ON coupon_receive.sid=coupon_logs.coupon_receive_sid JOIN member ON coupon_receive.member_sid=member.member_sid WHERE coupon_receive.member_sid=1 AND coupon_receive.end_time <NOW() AND coupon_receive.status=0 AND coupon_receive.end_time>0 ORDER BY end_time DESC;`;
         const [rows_type2_expired]=await db.query(sql1,[output.member_sid]);
         output.rows_type2_expired=rows_type2_expired;
 
 
-        const sql2=`SELECT coupon.coupon_name,coupon.coupon_money,coupon_receive.status,coupon_logs.used_time,coupon_receive.member_sid FROM coupon_receive JOIN coupon ON coupon_receive.coupon_sid=coupon.sid LEFT JOIN coupon_logs ON coupon_receive.sid=coupon_logs.coupon_receive_sid JOIN member ON coupon_receive.member_sid=member.member_sid WHERE coupon_receive.member_sid=1 AND coupon_receive.status=1 and coupon_logs.used_time>0`;
+        const sql2=`SELECT coupon.coupon_name,coupon.coupon_money,coupon_receive.status,coupon_logs.used_time,coupon_receive.member_sid FROM coupon_receive JOIN coupon ON coupon_receive.coupon_sid=coupon.sid LEFT JOIN coupon_logs ON coupon_receive.sid=coupon_logs.coupon_receive_sid JOIN member ON coupon_receive.member_sid=member.member_sid WHERE coupon_receive.member_sid=1 AND coupon_receive.status=1 and coupon_logs.used_time>0 ORDER BY used_time DESC;`;
         const [rows_type2_used]=await db.query(sql2,[output.member_sid]);
         output.rows_type2_used=rows_type2_used;
         return output;
